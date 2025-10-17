88a98da3e11d13b0ce04952d2a36b2132b357b74
SimbaGeti-hub <janetjorom02@gmail.com>
2025-10-17 13:21:02 +0300

    implementation of colors

--- simbaGeti/public/js/logIn.js
+++ simbaGeti/public/js/logIn.js
@@ -1,37 +1,3 @@
-// const loginEmail = document.getElementById('loginEmail');
-// const loginPassword = document.getElementById('loginPassword');
-// const loginBtn = document.getElementById('loginBtn');
-
-// loginBtn.addEventListener('click', () => {
-//   let hasError = false;
-
-//   if (!loginEmail.value.trim()) {
-//     loginEmail.classList.add('error');
-//     hasError = true;
-//   } else {
-//     loginEmail.classList.remove('error');
-//   }
-
-//   if (!loginPassword.value.trim()) {
-//     loginPassword.classList.add('error');
-//     hasError = true;
-//   } else {
-//     loginPassword.classList.remove('error');
-//   }
-
-//   if (!hasError) {
-//     window.location.href = 'success.html';
-//   }
-// });
-
-// loginEmail.addEventListener('input', () => {
-//   loginEmail.classList.remove('error');
-// });
-
-// loginPassword.addEventListener('input', () => {
-//   loginPassword.classList.remove('error');
-// });
-
 
 const loginEmail = document.getElementById('loginEmail');
 const loginPassword = document.getElementById('loginPassword');
